13b741c1c51123ed3437a204bb49e3a65f596c3d
DeyvisonTav <deyvisontav@gmail.com>
2024-05-25 23:12:42 -0300

    feat: added route for products

---
+++ src/app/categorias-produtos/page.tsx
@@ -0,0 +1,7 @@
+export default function CategoriasProdutos() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">Categorias Produtos 3° Milenio</h1>
+    </div>
+  );
+}
---
+++ src/components/table-products.tsx
@@ -0,0 +1,7 @@
+export function TableProductsComponent() {
+  return (
+    <div>
+      <h1 className="text-xl font-semibold">Produtos 3° Milenio</h1>
+    </div>
+  );
+}
