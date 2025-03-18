ea73781c0374c4fe69e133213ac2b11d732f6551
DevDeyvidTav <devdeyvid2022@gmail.com>
2025-03-17 21:32:28 -0300

    feat: vercel analytics

--- src/app/painel-de-controle/layout.tsx
+++ src/app/painel-de-controle/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { LayoutComponent } from "@/components/layout";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={inter.className}>
         <LayoutComponent>{children}</LayoutComponent>
+        <Analytics />
       </body>
     </html>
   );
